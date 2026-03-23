af61af6cbbff3183e6602352b4ae9f5532f7fdd4
YaayY360 <leyanissou@gmail.com>
2026-03-23 11:57:21 +0100

    modification map

--- src/RenderEngine.java
+++ src/RenderEngine.java
@@ -71,7 +71,7 @@ public class RenderEngine extends JPanel implements Engine{
                 // Affichage FPS
                 g.setColor(Color.YELLOW);
                 g.setFont(new Font("Arial", Font.BOLD, 14));
-                g.drawString("FPS : " + fps, 325, 20);
+                g.drawString("FPS : " + fps, 1780, 20);
 
                 paintChildren(g);
                 break;
